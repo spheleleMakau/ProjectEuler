93ff14dfcf7ee56405154202ce9e6af58ecbdd81
spheleleMakau <missmakau9@gmail.com>
2024-05-02 13:59:03 +0200

    added footer, change the content for the problems to be loaded in the main html

--- script.js
+++ script.js
@@ -11,8 +11,48 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+function loadContent(url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('loadedContent').innerHTML = data;
+        })
+        .catch(error => console.error('Error:', error));
+}
 
-// script.js
+function sumOfMultiples(limit, num1, num2) {
+    let sum = 0;
+    for (let i = 1; i < limit; i++) {
+        if (i % num1 === 0 || i % num2 === 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
 
-// script.js
-// script.js
+function checkAnswer() {
+    const userAnswer = parseInt(document.getElementById('total-input').value);
+    const limit = parseInt(document.getElementById('limit-input').value);
+    const num1 = parseInt(document.getElementById('num1-input').value);
+    const num2 = parseInt(document.getElementById('num2-input').value);
+    const correctAnswer = sumOfMultiples(limit, num1, num2);
+    var resultImage = document.getElementById('resultImage');
+   
+
+    if (userAnswer === correctAnswer) { // Check if the answer is correct
+        resultImage.style.display = 'block'; // Display the picture
+    
+    } else {
+        alert('Sorry, that is incorrect. Please try again.'); // Notify the user if the answer is incorrect
+    }
+}
+
+// Update placeholders with calculated values
+const limit = 1000;
+const num1 = 3;
+const num2 = 5;
+const multiplesSum = sumOfMultiples(limit, num1, num2);
+document.getElementById('list-placeholder').innerText = `list of multiples`;
+document.getElementById('sum-placeholder').innerText = `sum of multiples`;
+
+
